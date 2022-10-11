c79b7f3ca7bd17fe1c1f36f5430d2675ed6ed57b
edipo-moreira <41266040+edipo-moreira@users.noreply.github.com>
2022-10-10 22:08:27 -0300

    added translate to pr br

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import jsonServerProvider from 'ra-data-json-server'
 import { ProductList, ProductCreate, ProductEdit } from './components/products/index'
 import { MyAppBar } from './components/app-bar/app-bar'
 import { ProductHistoryCreate, ProductHistoryEdit, ProductHistoryList } from './components/products-history'
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import ptBrMessages from 'ra-language-pt-br';
 
 const dataProvider = jsonServerProvider('https://backend-ldc.herokuapp.com')
 const MyLayout = (props) => <Layout {...props} appBar={MyAppBar} />;
 
+const messages = {
+  'pt-br': ptBrMessages,
+};
+const i18nProvider = polyglotI18nProvider(locale => messages[locale], 'pt-br');
+
 function App() {
   return (
-   <Admin dataProvider={dataProvider} layout={MyLayout}>
+   <Admin i18nProvider={i18nProvider} dataProvider={dataProvider} layout={MyLayout}>
     <Resource options={{ label: 'Produtos' }} name='products' list={ProductList} create={ProductCreate} edit={ProductEdit} />
     <Resource options={{ label: 'Controle de Estoque' }} name='products-history' list={ProductHistoryList} create={ProductHistoryCreate} edit={ProductHistoryEdit} />
    </Admin>
